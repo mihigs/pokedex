38da5d1f1eb03c79a8fb7565a16102cb2eb723af
unknown <miralem.avdic@mss.ba>
2021-08-16 15:54:20 +0200

    Initialized VueRouter and pages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Home from './pages/Home'
+import PokemonList from './pages/PokemonList'
 
 Vue.config.productionTip = false
 
+// 1. Define route components.
+// These can be imported from other files
+// const Foo = { template: '<div>foo</div>' }
+// const Bar = { template: '<div>bar</div>' }
+
+// 2. Define some routes
+// Each route should map to a component. The "component" can
+// either be an actual component constructor created via
+// `Vue.extend()`, or just a component options object.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/allPokemon', component: PokemonList }
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Pokedex</h1>
+        <router-link to="/allPokemon">See all pokemon </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
